6d92635ddad177edc30d8dbfd18218097bfa933c
EdgarNegrin <alu0101210964@ull.edu.es>
2021-12-17 11:47:47 +0000

    Fallo en el calculo del TF-IDF

--- recomendador.py
+++ recomendador.py
@@ -1,3 +1,4 @@
+import math
 
 class recomendador:
   def __init__(self, fileName):
@@ -5,8 +6,11 @@ class recomendador:
     self.items = []
     self.tf = []
     self.idf = []
+    self.tf_idf = []
     self.loadFile(fileName)
     self.calculateTF()
+    self.calculateIDF()
+    self.calculateTFIDF()
     self.showInfo()
   
   # Cargar documentos
@@ -14,7 +18,7 @@ class recomendador:
     textFile = open(fileName, 'r')
     self.documents = (textFile.readlines()) ## almacena los saltos de linea / hay que eliminarlos
     for i in range(len(self.documents)):
-      self.documents[i] = self.documents[i].replace(".", "").replace(",", "")
+      self.documents[i] = self.documents[i].replace(".", "").replace(",", "").lower()
       self.items.append(self.documents[i].split())
   
   
@@ -26,29 +30,38 @@ class recomendador:
   def calculateIDF(self):
     # Documentos que pueden ser recomendados
     N = len(self.documents)
+    # Creacion de diccionario
+    items = []
+    for i in range(len(self.documents)):
+      items += self.items[i]
+    items = set(items)
+    self.idf = dict.fromkeys(items, 0.)
     # Numero de documentos en donde la palabra aparece
-    tempIdf = []
-    for doc in range(len(self.documents)):
-      for item in range(len(self.tf[doc])):
-        tempIdf.append(self.tf[doc][item][0])
-        for docSearch in range(len(self.documents)):
-          if self.tf[item] in self.items[docSearch]:
-            tempIdf[item].append(docSearch) 
-            break
+    for item in self.idf:
+      for docSearch in range(len(self.items)):
+        if item in self.items[docSearch]:
+          self.idf[item] += 1
     
-    for doc in range(len(self.documents)):
-      for idf in tempIdf:
-        self.idf.append(list(zip(set(), len(idf))))
+    for item in self.idf:
+      self.idf[item] = math.log10(N / self.idf[item])
     
     # tf/raiz(suma tfs al cuadrado) -> para hacer la similitud
+  
+  def calculateTFIDF(self): ## No almacena el orden correctamente, repite los mismos items
+    tf_idf = []
+    for doc in range(len(self.documents)):
+      for item in range(len(self.tf[doc])):
+        cal = self.tf[doc][item][1] * self.idf[self.tf[doc][item][0]]
+        tf_idf.append((self.tf[doc][item][0], cal))
+      self.tf_idf.append(tf_idf)
+    
   
   # Muestra por pantalla los resultados
   def showInfo(self):
     for doc in range(len(self.documents)):
-      print('{4}{0:6s}{4} {4}{1:24s}{4} {4}{2:3s}{4} {4}{3:3s}{4}'.format('Indice', 'Termino', 'TF', 'IDF', '|'))
+      print('{5}{0:6s}{5} {5}{1:30s}{5} {5}{2:3s}{5} {5}{3:3s}{5} {5}{4:3s}{5}'.format('Indice', 'Termino', 'TF', 'IDF', 'TF-IDF', '|'))
       print()
       for item in range(len(self.tf[doc])):
-        print('{4}{0:6d}{4} {4}{1:30s}{4} {4}{2:3d}{4} {4}{3:3s}{4}'.format(item, self.tf[doc][item][0], self.tf[doc][item][1], 'IDF', '|'))
+        print('{5}{0:6d}{5} {5}{1:30s}{5} {5}{2:3d}{5} {5}{3:3f}{5} {5}{4:3s}{5}'.format(item, self.tf[doc][item][0], self.tf[doc][item][1], self.idf[self.tf[doc][item][0]], self.tf_idf[doc][item][0], '|'))
       print("\n\n")
-  
   
